ef74f121282434c735ca36a38969eee078e537c4
KJ <122484032+plageous@users.noreply.github.com>
2026-04-23 11:56:49 -0700

    Added TestGetWithThreeElements. Fixed imports.

--- src/TestDynamicStringList.java
+++ src/TestDynamicStringList.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class TestDynamicStringList {
     // get()
-
+    @Test
+    void TestGetWithThreeElements() {
+        DynamicStringList list = new DynamicStringList();
+        list.add("Hello");
+        list.add("World");
+        list.add("!");
+        assertEquals("World", list.get(1));
+    }
     // set()
 
     // add()
