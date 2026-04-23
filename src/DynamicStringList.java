1d188f63bbfe67e6f3f9cb8576990c1bc8c475a6
KJ <122484032+plageous@users.noreply.github.com>
2026-04-23 11:25:24 -0700

    get() complete!

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -2,9 +2,17 @@ public class DynamicStringList implements StringList {
     private String[] array = new String[10];
     private int size = 0;
 
-
+    /**
+   * Retrieves the string at the specified index in the list.
+   *
+   * @param index the index of the string to retrieve.
+   * @return the string at the specified index.
+   * @throws IndexOutOfBoundsException if the index is out of range (index < 0 or index >= size()).
+   */
     public String get(int index) {
-        return null;
+        if (index >= size || index < 0) { throw new IndexOutOfBoundsException(); }
+        String value = array[index];
+        return value;
     }
 
     public void set(int index, String value) {
