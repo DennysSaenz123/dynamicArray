1c02143eb5e785701c99ed9ce5f2170a8fc79f26
KJ <122484032+plageous@users.noreply.github.com>
2026-04-23 11:19:45 -0700

    Populated DynamicStringList.java

---
+++ src/DynamicStringList.java
@@ -0,0 +1,27 @@
+public class DynamicStringList implements StringList {
+    
+    public String get(int index) {
+        return null;
+    }
+
+    public void set(int index, String value) {
+        
+    }
+
+    public void add(String value) {
+
+    }
+
+    public String remove(int index) {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int capacity() {
+        return 0;
+    }
+
+}
