be9233e19d9dff4f985c2af93878fc683b728ec3
KJ <122484032+plageous@users.noreply.github.com>
2026-04-23 11:35:52 -0700

    Added comments to set()

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -15,6 +15,13 @@ public class DynamicStringList implements StringList {
         return value;
     }
 
+    /**
+   * Replaces the string at the specified index with the given value.
+   *
+   * @param index the index of the string to replace.
+   * @param value the new value to set at the specified index.
+   * @throws IndexOutOfBoundsException if the index is out of range (index < 0 or index >= size()).
+   */
     public void set(int index, String value) {
         if (index >= size || index < 0) { throw new IndexOutOfBoundsException(); }
         array[index] = value;
